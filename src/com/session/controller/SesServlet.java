a6d3a733255ab6bf7d688d4314eda6805c1dc21b
hsujessie <j986608@gmail.com>
2021-03-01 17:48:15 +0800

    2021-03-01 add SesServlet.java

---
+++ src/com/session/controller/SesServlet.java
@@ -0,0 +1,23 @@
+package com.session.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@MultipartConfig()
+public class SesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;  
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		doPost(req,res);
+	}
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		System.out.println("action:"+action);
+	}
+
+}
